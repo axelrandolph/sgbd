ce880844562d5f399dd8fbd18c7e4132e75a1adb
Leslie <leslie.kiav@outlook.fr>
2019-11-29 18:06:08 +0100

    Creation de la classe utilisateur

--- src/Model/EntityKitchen.java
+++ src/Model/EntityKitchen.java
@@ -9,6 +9,14 @@ public class EntityKitchen {
      */
     private int nbGasPoint;
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * A copy Constructor for Kitchen object
+=======
+    /* Constructor*/
+
+>>>>>>> Stashed changes
     /**
      * The default constructor
      */
