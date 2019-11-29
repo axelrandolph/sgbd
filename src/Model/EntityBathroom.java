9a47ba55571c320c6c988f1f2ce6139cef773c83
Axel RANDOLPH <randolph.axel@yahoo.fr>
2019-11-29 15:59:53 +0100

    Class Bathroom

--- src/Model/EntityBathroom.java
+++ src/Model/EntityBathroom.java
@@ -1,12 +1,51 @@
 package Model;
 
-<<<<<<< Updated upstream
 public class EntityBathroom extends AbstractLocal{
 
+    /**
+     * Number of water point in the bathroom
+     */
     private int nbWaterPoint;
 
-=======
-public class EntityBathroom {
->>>>>>> Stashed changes
+    /**
+     * A copy Bathroom Constructor
+     * @param idLocal
+     * @param appartment
+     * @param description
+     * @param area
+     * @param nbWaterPoint
+     */
+    public EntityBathroom(int idLocal, EntityAppartment appartment, String description, float area, int nbWaterPoint) {
+        super(idLocal, appartment, description, area);
+        this.nbWaterPoint = nbWaterPoint;
+    }
+
+    /**
+     *A copy Constructor
+     * @param appartment
+     * @param nbWaterPoint
+     */
+    public EntityBathroom(EntityAppartment appartment, int nbWaterPoint) {
+        super(appartment);
+        this.nbWaterPoint = nbWaterPoint;
+    }
+
+    /**
+     * Default Bathroom Constructor
+     */
+    public EntityBathroom() {
+    }
+
+    /*
+    Getters and Setters
+     */
+    public int getNbWaterPoint() {
+        return nbWaterPoint;
+    }
+
+    public void setNbWaterPoint(int nbWaterPoint) {
+        this.nbWaterPoint = nbWaterPoint;
+    }
+
 
 }
